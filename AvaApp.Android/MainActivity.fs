21955fbd5ce9a5d9de5ab54e8312a5b2a569dece
Hadrian Tang <hadrianwttang@outlook.com>
2023-02-02 14:19:18 +0800

    Update to v11.0.0-preview4

--- AvaApp.Android/MainActivity.fs
+++ AvaApp.Android/MainActivity.fs
@@ -6,6 +6,4 @@ open Avalonia
 
 [<Activity(Label = "AvaApp.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = (ConfigChanges.Orientation ||| ConfigChanges.ScreenSize))>]
 type MainActivity() =
-    inherit AvaloniaActivity<AvaApp.App>()
-    override this.CustomizeAppBuilder builder =
-        base.CustomizeAppBuilder builder
+    inherit AvaloniaMainActivity()
--- AvaApp.Android/SplashActivity.fs
+++ AvaApp.Android/SplashActivity.fs
@@ -1,11 +1,14 @@
 ﻿namespace AvaApp.Android
 open Android.App
 open Android.Content
+open Avalonia.Android
 type Application = Android.App.Application
 
 [<Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)>]
 type SplashActivity() =
-    inherit Activity()
+    inherit AvaloniaSplashActivity<AvaApp.App>()
+    override this.CustomizeAppBuilder builder =
+        base.CustomizeAppBuilder builder
     override this.OnResume() =
         base.OnResume()
         this.StartActivity(new Intent(Application.Context, typeof<MainActivity>))
